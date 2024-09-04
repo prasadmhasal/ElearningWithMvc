883e58d668fbfbc2c76ae23ff74a39306b9571eb
prasad <prasadmhasal@gmail.com>
2024-09-04 23:29:32 +0530

    feedback

--- ElearningWithMvc/wwwroot/js/site.js
+++ ElearningWithMvc/wwwroot/js/site.js
@@ -1,4 +1,7 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿$(document).ready(function () {
+    // Activate the tabs with Bootstrap's Tab plugin
+    $('#ex1 a[data-bs-toggle="tab"]').on('click', function (e) {
+        e.preventDefault();
+        $(this).tab('show');
+    });
+});
